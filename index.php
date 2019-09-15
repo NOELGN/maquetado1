e35cd6466becbfe51e53403fffb928b5997c6b35
noe <noelgn@outlook.com>
2019-09-14 23:02:12 -0500

    primer version

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>titulo de la pagina</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header id="encabezado">
+        <section id="logo">
+            <h1>logo</h1>
+        </section>
+        <section id="titulo">
+            <h1>titulo</h1>
+        </section>
+        <section id="busqueda">
+            <h1>busqueda</h1>
+        </section>
+    </header>
+
+    <nav>
+        <h1>nav</h1>
+    </nav>
+
+    <main>
+        <section id="contenido1">
+            <article class="secciones">
+                <h4>contenido1</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido2</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido3</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido4</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido5</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido6</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido7</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido8</h4>
+            </article>
+            <article class="secciones">
+                <h4>contenido9</h4>
+            </article>
+        </section>
+
+        <section id="contenido2">
+            <aside id="contenidoAside">
+                <h3>aside</h3>
+            </aside>
+        </section>
+
+    </main>
+
+    <footer id="piePagina">
+        <h2>footer</h2>
+    </footer>
+
+</body>
+
+</html>
